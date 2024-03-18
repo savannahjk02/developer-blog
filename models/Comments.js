3f93b31171116460217bf16cb6b5a6b1182de076
savannahjk02 <savannahjk02@gmail.com>
2024-03-17 20:13:14 -0400

    models

---
+++ models/Comments.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,13 +1,30 @@
 const User = require('./User');
-const Project = require('./Blog');
+const Blog = require('./Blog');
+const Comments = require('./Comments');
 
-User.hasMany(Project, {
+User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comments.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comments, {
+  foreignKey: 'blog_id',
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comments };
